6b75e19508d66c8eb5a05cd0fa52b98393ccb133
franciscocaballero <caballerofrancisco22@yahoo.com>
2017-08-21 12:56:06 -0400

    added sticky nav

---
+++ app.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+$('.section-features').waypoint(function(direction) {
+  if (direction == 'down') {
+    $('nav').addClass('sticky')
+  } else {
+    $('nav').removeClass('sticky');
+   }
+  }, {
+    offset: '60px'
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+});
